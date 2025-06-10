3033bffe7d099559b7c541c8a6fdb4cdc9c61c3d
Verbrugghejara <jara.verbrugghe@student.howest.be>
2025-06-10 15:57:18 +0200

    add: abby arrow

--- abbyStorage/src/screens/event/EventDetail.vue
+++ abbyStorage/src/screens/event/EventDetail.vue
@@ -3,7 +3,6 @@
     <!-- Header -->
     <nav class="relative flex items-center justify-between p-4 bg-alphaYellow">
       <svg
-        @click="goBack"
         xmlns="http://www.w3.org/2000/svg"
         width="21"
         height="18"
