352a4286bdfb83de20b92e592b940aed71ee9329
TiemenDhondt <tiemen.dhondt@icloud.com>
2025-06-16 16:09:12 +0200

    Fix: Try 4 for deployment

--- abbyStorage/vite.config.ts
+++ abbyStorage/vite.config.ts
@@ -1,9 +1,10 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // stel de juiste base in voor GitHub Pages
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   base: '/Integration-4/' // vervang dit met je repo-naam
 })
